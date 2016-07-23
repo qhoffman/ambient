26e3b18897fcfd379e63a2310530b1f61399494b
Quay <qhoffman@gmail.com>
2016-07-23 12:23:23 +0000

    trying to cache some things with SW, saved edits to manifest

--- sw-precache-config.js
+++ sw-precache-config.js
@@ -2,7 +2,25 @@ module.exports = {
   staticFileGlobs: [
     '/index.html',
     '/manifest.json',
-    '/bower_components/webcomponentsjs/webcomponents-lite.min.js'
+    '/bower_components/webcomponentsjs/webcomponents-lite.min.js',
+    '/src/my-view1.html',
+    '/media/*.html'
   ],
+  runtimeCaching: [{
+    urlPattern: /\/bower_components\//,
+    handler: 'fastest'
+  }, {
+    urlPattern: /\/images\//,
+    handler: 'fastest'
+  }, {
+    urlPattern: /\/media\//,
+    handler: 'cacheFirst',
+    options: {
+      cache: {
+        maxEntries: 10,
+        name: 'sounds-cache'
+      }
+    }
+  }]
   navigateFallback: '/index.html'
 };
